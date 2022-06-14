3d324bc0bb00e57b95eea2bc741016b3c3e07ac3
infernostars <blazem7708.2@gmail.com>
2022-06-14 13:12:44 -0600

    Add lark parsing lib, pt. 1

---
+++ src/interpreter/lark.py
@@ -0,0 +1,42 @@
+import lark
+
+bstargrammar = r"""
+start: arg*
+    
+?arg:
+    | "true" -> true
+    | "false" -> false
+    | string
+    | SIGNED_NUMBER -> number
+    | function
+    | array
+    | C_COMMENT -> comment
+    | unescaped_string
+
+string: ESCAPED_STRING
+
+block: ALPHANUMERIC
+args: arg*
+
+array: "{" [arg ("," arg)*] "}"
+
+function: ("[") block args ("]")
+
+unescaped_string: ALPHANUMERIC
+
+
+DIGIT: "0".."9"
+LCASE_LETTER: "a".."z"
+UCASE_LETTER: "A".."Z"
+LETTER: UCASE_LETTER | LCASE_LETTER
+ALPHANUMERIC: ("_" | "." | LETTER | DIGIT)+
+
+
+// imports from common library my beloved
+%import common.WORD
+%import common.ESCAPED_STRING
+%import common.SIGNED_NUMBER
+%import common.C_COMMENT
+
+%import common.WS
+%ignore WS"""
