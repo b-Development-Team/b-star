17deff3f412e8fb99ab4a200ffe4781717cd5a95
Inferno <blazem7708.2@gmail.com>
2022-06-18 13:40:12 -0600

    Porser

--- src/interpreter/parse.py
+++ src/interpreter/parse.py
@@ -7,44 +7,35 @@ start: bstar*
     | function
     | ALLBUTBRACKETS
 ?arg:
-    | "true" -> true
-    | "false" -> false
+    | ("true" | "True") -> true
+    | ("false" | "False") -> false
     | string
-    | SIGNED_NUMBER -> number
+    | SIGNED_INT -> integer
+    | DECIMAL -> float
     | function
     | array
     | unescaped_string
-
 string: ESCAPED_STRING
-
 block: ALPHANUMERIC
 args: arg*
-
 array: "{" [arg ("," arg)*] "}"
-
 function: ("[") block args ("]")
-
 unescaped_string: ALPHANUMERIC
 ALLBUTBRACKETS: ALLEXCEPTBRACKETS+
-
-
 DIGIT: "0".."9"
 LCASE_LETTER: "a".."z"
 UCASE_LETTER: "A".."Z"
 LETTER: UCASE_LETTER | LCASE_LETTER
 ALPHANUMERIC: (ALLNONCONFLICTING)+
-ALLNONCONFLICTING: /[^\[\]0-9\{\}\"\s]/
+ALLNONCONFLICTING: /[^\[\]0-9\{\}\"\s\,]/
 ALLEXCEPTBRACKETS: /[^\[\]]/
 
-
 // imports from common library my beloved
-%import common.WORD
 %import common.ESCAPED_STRING
-%import common.SIGNED_NUMBER
-
+%import common.SIGNED_INT
+%import common.DECIMAL
 %import common.C_COMMENT
 %import common.WS
-
 %ignore WS
 %ignore C_COMMENT"""
 parser = Lark(bstargrammar)
